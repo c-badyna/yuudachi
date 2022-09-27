f9a171ac01c7e9cd5afa1685a87c02163ecb0983
vike.y <99621386+vikey3428@users.noreply.github.com>
2022-09-27 19:44:40 +0900

    Add files via upload

---
+++ typings/index.d.ts
@@ -0,0 +1,87 @@
+/**
+ * SFW:
+ */
+declare module "akaneko" {
+  /**
+   * @returns neko... Images ! 
+   */
+  export function neko(): string;
+
+  /**
+   * @returns lewdneko... Images !
+   */
+  export function lewdNeko(): string;
+
+  /**
+   * @returns foxgirl... Images !
+   */
+  export function foxgirl(): string;
+
+  /**
+   * @returns wallpepers... Images !
+   */
+  export function wallpapers(): string;
+
+  /**
+   * @returns mobile wallpapers... Images !
+   */
+  export function mobileWallpapers(): string;
+
+  /**
+   * NSFW:
+   */
+  namespace nsfw {
+
+    export function ass(): string;
+
+    export function bdsm(): string;
+
+    export function cum(): string;
+
+    export function doujin(): string;
+
+    export function feet(): string;
+
+    export function femdom(): string;
+
+    export function foxgirl(): string;
+
+    export function gifs(): string;
+
+    export function glasses(): string;
+
+    export function hentai(): string;
+
+    export function netorare(): string;
+
+    export function maid(): string;
+
+    export function masturbation(): string;
+
+    export function orgy(): string;
+
+    export function panties(): string;
+
+    export function pussy(): string;
+
+    export function school(): string;
+
+    export function tentacles(): string;
+
+    export function thighs(): string;
+
+    export function uglyBastard(): string;
+
+    export function uniform(): string;
+
+    export function yuri(): string;
+
+    export function zettaiRyouiki(): string;
+
+    export function mobileWallpapers(): string;
+
+    export function wallpapers(): string;
+
+    export function succubus(): string;
+  }
+}
